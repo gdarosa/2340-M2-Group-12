4e887b95071e324caff95a5e256111b32c54b98b
David Ho <davidho1837@gmail.com>
2018-01-31 00:52:52 -0500

    Changed Person4.java implementation and author line

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -32,7 +32,7 @@ public class Person4 {
     private String calc(String input) {
         String value = "";
         for (int i = 0; i < input.length(); i++) {
-            value = value + ((String) (input.charAt(i) + 1));
+            value = value + ((char) (input.charAt(i) + 1));
         }
         return value;
     }
